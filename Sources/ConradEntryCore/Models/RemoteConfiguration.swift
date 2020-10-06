7a17b139544e6eb385299d78d6c925e3d331b003
Jesse Halley <halley.jesse@me.com>
2020-10-06 14:06:51 +1100

    Add inspection companies to config

--- Sources/ConradEntryCore/Models/RemoteConfiguration.swift
+++ Sources/ConradEntryCore/Models/RemoteConfiguration.swift
@@ -4,9 +4,10 @@
 //
 
 public struct RemoteConfiguration: Codable {
-    public init(smelter: [String], dischargeMethod: [String], samplingCompany: [String], sampleReductionLocation: [String], qualityLotSamplePreparationPulverizer: [String], sampleReductionGridSize: [String], sampleReductionMethod: [String], samplingPoint: [String], qualityLotSamplePreparationDivisionMethod: [String], qualityLotSamplePreparationScreenAperture: [String], client: [String], samplingTransparency: [String], trader: [String], samplingLocation: [String], samplingMethod: [String], qualityLotSamplePreparationMethod: [String], weighingMethod: [WeighingMethod], port: [Port]) {
+    public init(smelter: [String], dischargeMethod: [String], inspectionCompany: [String], samplingCompany: [String], sampleReductionLocation: [String], qualityLotSamplePreparationPulverizer: [String], sampleReductionGridSize: [String], sampleReductionMethod: [String], samplingPoint: [String], qualityLotSamplePreparationDivisionMethod: [String], qualityLotSamplePreparationScreenAperture: [String], client: [String], samplingTransparency: [String], trader: [String], samplingLocation: [String], samplingMethod: [String], qualityLotSamplePreparationMethod: [String], weighingMethod: [WeighingMethod], port: [Port]) {
         self.smelter = smelter
         self.dischargeMethod = dischargeMethod
+        self.inspectionCompany = inspectionCompany
         self.samplingCompany = samplingCompany
         self.sampleReductionLocation = sampleReductionLocation
         self.qualityLotSamplePreparationPulverizer = qualityLotSamplePreparationPulverizer
@@ -25,13 +26,14 @@ public struct RemoteConfiguration: Codable {
         self.port = port
     }
 
-    public let smelter, dischargeMethod, samplingCompany, sampleReductionLocation, qualityLotSamplePreparationPulverizer, sampleReductionGridSize, sampleReductionMethod, samplingPoint, qualityLotSamplePreparationDivisionMethod, qualityLotSamplePreparationScreenAperture, client, samplingTransparency, trader, samplingLocation, samplingMethod, qualityLotSamplePreparationMethod: [String]
+    public let smelter, dischargeMethod, inspectionCompany, samplingCompany, sampleReductionLocation, qualityLotSamplePreparationPulverizer, sampleReductionGridSize, sampleReductionMethod, samplingPoint, qualityLotSamplePreparationDivisionMethod, qualityLotSamplePreparationScreenAperture, client, samplingTransparency, trader, samplingLocation, samplingMethod, qualityLotSamplePreparationMethod: [String]
     public let weighingMethod: [WeighingMethod]
     public let port: [Port]
 
     public enum CodingKeys: String, CodingKey {
         case smelter, client, trader, port
         case dischargeMethod = "discharge_method"
+        case inspectionCompany = "inspection_company"
         case samplingCompany = "sampling_company"
         case sampleReductionLocation = "sample_reduction_location"
         case qualityLotSamplePreparationPulverizer = "quality_lot_sample_preparation_pulverizer"
