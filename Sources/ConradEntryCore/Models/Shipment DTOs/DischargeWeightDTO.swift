8dac96adc28501290620bbdff2df7abcc2a94c6c
Jesse Halley <halley.jesse@me.com>
2020-11-03 11:49:06 +1100

    Fix abcCheck and calibrationCheck parameters being non-optional in the DischargeWeightDTO initialiser

--- Sources/ConradEntryCore/Models/Shipment DTOs/DischargeWeightDTO.swift
+++ Sources/ConradEntryCore/Models/Shipment DTOs/DischargeWeightDTO.swift
@@ -19,7 +19,7 @@ public struct DischargeWeightDTO: Codable {
     public let abcCheck: ABCCheck?
     public let comments: String
 
-    public init(id: UUID?, methodID: UUID, wetMetricTonnes: Double, moisturePercentage: Double, dryMetricTonnes: Double, isSettlementWeight: Bool, equipmentName: String?, equipmentModel: String?, equipmentCertificationDate: Date?, calibrationCheck: CalibrationCheck, abcCheck: ABCCheck, comments: String) {
+    public init(id: UUID?, methodID: UUID, wetMetricTonnes: Double, moisturePercentage: Double, dryMetricTonnes: Double, isSettlementWeight: Bool, equipmentName: String?, equipmentModel: String?, equipmentCertificationDate: Date?, calibrationCheck: CalibrationCheck?, abcCheck: ABCCheck?, comments: String) {
         self.id = id
         self.methodID = methodID
         self.wetMetricTonnes = wetMetricTonnes
