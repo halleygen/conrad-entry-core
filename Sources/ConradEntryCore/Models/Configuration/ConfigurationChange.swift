d2b2014217114424f4c9ee9f5876fbf4b06a918e
Jesse Halley <halley.jesse@me.com>
2020-11-20 09:26:07 +1100

    Add remote configuration change struct so that configurations can be added or updated via the API

--- Sources/ConradEntryCore/Models/Configuration/ConfigurationChange.swift
+++ Sources/ConradEntryCore/Models/Configuration/ConfigurationChange.swift
@@ -5,31 +5,51 @@
 
 import Foundation
 
-// public extension RemoteConfiguration {
-//    struct Change: Codable {
-//        public let table: String
-//        public let column: String
-//        public let data: Data
-//        public let operation: Operation
-//
-//        public static func deleting(_ data: Data, fromItemNamed key: String) -> Change {
-//            self.init(key: key, data: data, operation: .delete)
-//        }
-//
-//        public static func updating(_ data: Data, fromItemNamed key: String) -> Change {
-//            self.init(key: key, data: data, operation: .update)
-//        }
-//    }
-// }
-//
-// public extension RemoteConfiguration.Change {
-//    enum Operation: String, Codable {
-//        case update, delete
-//    }
-// }
-//
-// extension RemoteConfiguration.Change: CustomStringConvertible {
-//    public var description: String {
-//        "ConfigurationChange: \(operation) \"\(String(data: data, encoding: .utf8) ?? "[corrupted data]")\" from table '\(key)'"
-//    }
-// }
+ public extension RemoteConfiguration {
+    struct Change: Codable {
+        public let schema: String
+        public let data: Data
+        public let operation: Operation
+        
+        public init(schema: String, data: Data, operation: Operation) {
+            self.schema = schema
+            self.data = data
+            self.operation = operation
+        }
+    }
+ }
+
+ public extension RemoteConfiguration.Change {
+    enum Operation: Codable {
+        case add, update(primaryKey: Int)
+        
+        public enum CodingKeys: String, CodingKey {
+            case add, update
+        }
+        
+        public init(from decoder: Decoder) throws {
+            let container = try decoder.container(keyedBy: CodingKeys.self)
+            if container.contains(.add) {
+                self = .add
+            } else if let primaryKey = try container.decodeIfPresent(Int.self, forKey: .update) {
+                self = .update(primaryKey: primaryKey)
+            } else {
+                throw DecodingError.dataCorrupted(DecodingError.Context(codingPath: [], debugDescription: "Container did not contain either add or update key."))
+            }
+        }
+        
+        public func encode(to encoder: Encoder) throws {
+            var container = encoder.container(keyedBy: CodingKeys.self)
+            switch self {
+            case .add: try container.encodeNil(forKey: .add)
+            case .update(let primaryKey): try container.encode(primaryKey, forKey: .update)
+            }
+        }
+    }
+ }
+
+ extension RemoteConfiguration.Change: CustomStringConvertible {
+    public var description: String {
+        "ConfigurationChange: \(operation) in table '\(schema)'"
+    }
+ }
