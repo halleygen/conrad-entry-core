064f11a47901c9144e73604b851742c49ca97411
Jesse Halley <halley.jesse@me.com>
2020-11-20 15:26:35 +1100

    Rename properties of RemoteConfiguration to be plural

--- Sources/ConradEntryCore/Models/Configuration/RemoteConfiguration.swift
+++ Sources/ConradEntryCore/Models/Configuration/RemoteConfiguration.swift
@@ -4,73 +4,73 @@
 //
 
 public struct RemoteConfiguration: Codable {
-    public init(smelter: [SimpleConstant], dischargeMethod: [SimpleConstant], inspectionCompany: [SimpleConstant], samplingCompany: [SimpleConstant], sampleReductionLocation: [SimpleConstant], qualityLotSamplePreparationPulverizer: [SimpleConstant], sampleReductionGridSize: [SimpleConstant], sampleReductionMethod: [SimpleConstant], qualityLotSamplePreparationDivisionMethod: [SimpleConstant], qualityLotSamplePreparationScreenAperture: [SimpleConstant], client: [SimpleConstant], trader: [SimpleConstant], samplingLocation: [SimpleConstant], samplingMethod: [SimpleConstant], qualityLotSamplePreparationMethod: [SimpleConstant], samplingTransparency: [SamplingTransparency], samplingPoint: [SamplingPoint], weighingMethod: [WeighingMethod], maritimePort: [MaritimePort]) {
-        self.smelter = smelter
-        self.dischargeMethod = dischargeMethod
-        self.inspectionCompany = inspectionCompany
-        self.samplingCompany = samplingCompany
-        self.sampleReductionLocation = sampleReductionLocation
-        self.qualityLotSamplePreparationPulverizer = qualityLotSamplePreparationPulverizer
-        self.sampleReductionGridSize = sampleReductionGridSize
-        self.sampleReductionMethod = sampleReductionMethod
-        self.samplingPoint = samplingPoint
-        self.qualityLotSamplePreparationDivisionMethod = qualityLotSamplePreparationDivisionMethod
-        self.qualityLotSamplePreparationScreenAperture = qualityLotSamplePreparationScreenAperture
-        self.client = client
-        self.samplingTransparency = samplingTransparency
-        self.trader = trader
-        self.samplingLocation = samplingLocation
-        self.samplingMethod = samplingMethod
-        self.qualityLotSamplePreparationMethod = qualityLotSamplePreparationMethod
-        self.weighingMethod = weighingMethod
-        self.maritimePort = maritimePort
+    public init(smelters: [SimpleConstant], dischargeMethods: [SimpleConstant], inspectionCompanies: [SimpleConstant], samplingCompanies: [SimpleConstant], sampleReductionLocations: [SimpleConstant], qualityLotSamplePreparationPulverizers: [SimpleConstant], sampleReductionGridSizes: [SimpleConstant], sampleReductionMethods: [SimpleConstant], qualityLotSamplePreparationDivisionMethods: [SimpleConstant], qualityLotSamplePreparationScreenApertures: [SimpleConstant], clients: [SimpleConstant], traders: [SimpleConstant], samplingLocations: [SimpleConstant], samplingMethods: [SimpleConstant], qualityLotSamplePreparationMethods: [SimpleConstant], samplingTransparencies: [SamplingTransparency], samplingPoints: [SamplingPoint], weighingMethods: [WeighingMethod], maritimePorts: [MaritimePort]) {
+        self.smelters = smelters
+        self.dischargeMethods = dischargeMethods
+        self.inspectionCompanies = inspectionCompanies
+        self.samplingCompanies = samplingCompanies
+        self.sampleReductionLocations = sampleReductionLocations
+        self.qualityLotSamplePreparationPulverizers = qualityLotSamplePreparationPulverizers
+        self.sampleReductionGridSizes = sampleReductionGridSizes
+        self.sampleReductionMethods = sampleReductionMethods
+        self.samplingPoints = samplingPoints
+        self.qualityLotSamplePreparationDivisionMethods = qualityLotSamplePreparationDivisionMethods
+        self.qualityLotSamplePreparationScreenApertures = qualityLotSamplePreparationScreenApertures
+        self.clients = clients
+        self.samplingTransparencies = samplingTransparencies
+        self.traders = traders
+        self.samplingLocations = samplingLocations
+        self.samplingMethods = samplingMethods
+        self.qualityLotSamplePreparationMethods = qualityLotSamplePreparationMethods
+        self.weighingMethods = weighingMethods
+        self.maritimePorts = maritimePorts
     }
 
     public init() {
-        self.smelter = []
-        self.dischargeMethod = []
-        self.inspectionCompany = []
-        self.samplingCompany = []
-        self.sampleReductionLocation = []
-        self.qualityLotSamplePreparationPulverizer = []
-        self.sampleReductionGridSize = []
-        self.sampleReductionMethod = []
-        self.samplingPoint = []
-        self.qualityLotSamplePreparationDivisionMethod = []
-        self.qualityLotSamplePreparationScreenAperture = []
-        self.client = []
-        self.samplingTransparency = []
-        self.trader = []
-        self.samplingLocation = []
-        self.samplingMethod = []
-        self.qualityLotSamplePreparationMethod = []
-        self.weighingMethod = []
-        self.maritimePort = []
+        self.smelters = []
+        self.dischargeMethods = []
+        self.inspectionCompanies = []
+        self.samplingCompanies = []
+        self.sampleReductionLocations = []
+        self.qualityLotSamplePreparationPulverizers = []
+        self.sampleReductionGridSizes = []
+        self.sampleReductionMethods = []
+        self.samplingPoints = []
+        self.qualityLotSamplePreparationDivisionMethods = []
+        self.qualityLotSamplePreparationScreenApertures = []
+        self.clients = []
+        self.samplingTransparencies = []
+        self.traders = []
+        self.samplingLocations = []
+        self.samplingMethods = []
+        self.qualityLotSamplePreparationMethods = []
+        self.weighingMethods = []
+        self.maritimePorts = []
     }
 
-    public var smelter, dischargeMethod, inspectionCompany, samplingCompany, sampleReductionLocation, qualityLotSamplePreparationPulverizer, sampleReductionGridSize, sampleReductionMethod, qualityLotSamplePreparationDivisionMethod, qualityLotSamplePreparationScreenAperture, client, trader, samplingLocation, samplingMethod, qualityLotSamplePreparationMethod: [SimpleConstant]
-    public var samplingPoint: [SamplingPoint]
-    public var samplingTransparency: [SamplingTransparency]
-    public var weighingMethod: [WeighingMethod]
-    public var maritimePort: [MaritimePort]
+    public var smelters, dischargeMethods, inspectionCompanies, samplingCompanies, sampleReductionLocations, qualityLotSamplePreparationPulverizers, sampleReductionGridSizes, sampleReductionMethods, qualityLotSamplePreparationDivisionMethods, qualityLotSamplePreparationScreenApertures, clients, traders, samplingLocations, samplingMethods, qualityLotSamplePreparationMethods: [SimpleConstant]
+    public var samplingPoints: [SamplingPoint]
+    public var samplingTransparencies: [SamplingTransparency]
+    public var weighingMethods: [WeighingMethod]
+    public var maritimePorts: [MaritimePort]
 
     public enum CodingKeys: String, CodingKey, Codable {
-        case smelter, client, trader
-        case maritimePort = "maritime_port"
-        case dischargeMethod = "discharge_method"
-        case inspectionCompany = "inspection_company"
-        case samplingCompany = "sampling_company"
-        case sampleReductionLocation = "sample_reduction_location"
-        case qualityLotSamplePreparationPulverizer = "quality_lot_sample_preparation_pulverizer"
-        case sampleReductionGridSize = "sample_reduction_grid_size"
-        case sampleReductionMethod = "sample_reduction_method"
-        case weighingMethod = "weighing_method"
-        case samplingPoint = "sampling_point"
-        case qualityLotSamplePreparationDivisionMethod = "quality_lot_sample_preparation_division_method"
-        case qualityLotSamplePreparationScreenAperture = "quality_lot_sample_preparation_screen_aperture"
-        case samplingTransparency = "sampling_transparency"
-        case samplingLocation = "sampling_location"
-        case samplingMethod = "sampling_method"
-        case qualityLotSamplePreparationMethod = "quality_lot_sample_preparation_method"
+        case smelters, clients, traders
+        case maritimePorts = "maritime_ports"
+        case dischargeMethods = "discharge_methods"
+        case inspectionCompanies = "inspection_companies"
+        case samplingCompanies = "sampling_companies"
+        case sampleReductionLocations = "sample_reduction_locations"
+        case qualityLotSamplePreparationPulverizers = "quality_lot_sample_preparation_pulverizers"
+        case sampleReductionGridSizes = "sample_reduction_grid_sizes"
+        case sampleReductionMethods = "sample_reduction_methods"
+        case weighingMethods = "weighing_methods"
+        case samplingPoints = "sampling_points"
+        case qualityLotSamplePreparationDivisionMethods = "quality_lot_sample_preparation_division_methods"
+        case qualityLotSamplePreparationScreenApertures = "quality_lot_sample_preparation_screen_apertures"
+        case samplingTransparencies = "sampling_transparencies"
+        case samplingLocations = "sampling_locations"
+        case samplingMethods = "sampling_methods"
+        case qualityLotSamplePreparationMethods = "quality_lot_sample_preparation_methods"
     }
 }
