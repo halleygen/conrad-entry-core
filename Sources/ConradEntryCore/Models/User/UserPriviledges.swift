0a2b1262770fa5a929afd966e5a10a14c5a6fd1b
Jesse Halley <halley.jesse@me.com>
2021-02-03 11:00:43 +1100

    Mark User.Privilege as @objc so it can be used with CoreData

--- Sources/ConradEntryCore/Models/User/UserPriviledges.swift
+++ Sources/ConradEntryCore/Models/User/UserPriviledges.swift
@@ -6,6 +6,7 @@
 import Foundation
 
 public extension User {
+    @objc
     enum Privilege: Int, CaseIterable, Codable {
         case readAllShipments, writeAllShipments, editConfigurationValues, manageUsers
     }
