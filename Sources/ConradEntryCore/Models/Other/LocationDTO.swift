f59c35fc8b17caea47e6cf916687ad94d0bcc8bc
Jesse Halley <halley.jesse@me.com>
2021-03-02 08:31:47 +1100

    Remove LocationDTO.Coordinate struct and store latitude and longitude directly in LocationDTO as two separate properties

--- Sources/ConradEntryCore/Models/Other/LocationDTO.swift
+++ Sources/ConradEntryCore/Models/Other/LocationDTO.swift
@@ -7,11 +7,14 @@ import Foundation
 
 /// The latitude, longitude, and altitude reported by the system.
 public struct LocationDTO: Hashable, Codable {
-    /// The geographical coordinate information.
-    public var coordinate: Coordinate
+    /// The latitude in degrees.
+    public var latitude: Double
+
+    /// The longitude in degrees.
+    public var longitude: Double
 
     /// The altitude, measured in meters.
-    public var altitude: Double
+    public var altitude: Double?
 
     /// The radius of uncertainty for the location, measured in meters.
     public var horizontalAccuracy: Double
@@ -22,32 +25,17 @@ public struct LocationDTO: Hashable, Codable {
     /// The time at which this location was determined.
     public var timestamp: Date
 
-    public init(coordinate: Coordinate, altitude: Double, horizontalAccuracy: Double, verticalAccuracy: Double, timestamp: Date) {
-        self.coordinate = coordinate
+    public init(latitude: Double, longitude: Double, altitude: Double?, horizontalAccuracy: Double, verticalAccuracy: Double, timestamp: Date) {
+        self.latitude = latitude
+        self.longitude = longitude
         self.altitude = altitude
         self.horizontalAccuracy = horizontalAccuracy
         self.verticalAccuracy = verticalAccuracy
         self.timestamp = timestamp
     }
-}
 
-public extension LocationDTO {
-    /// The latitude and longitude associated with a location, specified using the WGS 84 reference frame.
-    struct Coordinate: Hashable, Codable {
-        /// The latitude in degrees.
-        public var latitude: Double
-
-        /// The longitude in degrees.
-        public var longitude: Double
-
-        /// Returns a Boolean value indicating whether the specified coordinate is valid.
-        public var isValid: Bool {
-            (-90 ... 90).contains(latitude) && (-180 ... 180).contains(longitude)
-        }
-
-        public init(latitude: Double, longitude: Double) {
-            self.latitude = latitude
-            self.longitude = longitude
-        }
+    /// Returns a Boolean value indicating whether the location's latitude and longitude values form valid coordinate.
+    public var isValid: Bool {
+        (-90 ... 90).contains(latitude) && (-180 ... 180).contains(longitude)
     }
 }
