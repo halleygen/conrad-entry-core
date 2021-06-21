f53ff15f9cb1ded81720760d710a8b3452866f32
Jesse Halley <halley.jesse@me.com>
2021-06-21 11:01:57 +1000

    Add export data privilege Remove unused objects

--- Sources/ConradEntryCore/Models/User/UserPriviledges.swift
+++ Sources/ConradEntryCore/Models/User/UserPriviledges.swift
@@ -8,12 +8,12 @@ import Foundation
 public extension User {
     #if os(Linux)
         enum Privilege: Int, CaseIterable, Codable {
-            case readAllShipments, writeAllShipments, editConfigurationValues, manageUsers
+            case readAllShipments, writeAllShipments, editConfigurationValues, manageUsers, exportData
         }
     #else
         @objc
         enum Privilege: Int, CaseIterable, Codable {
-            case readAllShipments, writeAllShipments, editConfigurationValues, manageUsers
+            case readAllShipments, writeAllShipments, editConfigurationValues, manageUsers, exportData
         }
     #endif
 }
@@ -29,6 +29,7 @@ extension User.Privilege: CustomStringConvertible {
         case .writeAllShipments: return NSLocalizedString("user.privileges.write-all-shipments", bundle: .module, comment: "")
         case .editConfigurationValues: return NSLocalizedString("user.privileges.edit-configuration-values", bundle: .module, comment: "")
         case .manageUsers: return NSLocalizedString("user.privileges.manage-users", bundle: .module, comment: "")
+        case .exportData: return NSLocalizedString("user.privileges.export-data", bundle: .module, comment: "")
         }
     }
 }
