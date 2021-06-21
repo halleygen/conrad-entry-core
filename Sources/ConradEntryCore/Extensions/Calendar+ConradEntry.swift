3a27d56ed6d79dee232559ca8dfc78a201ca89cb
Jesse Halley <halley.jesse@me.com>
2021-06-21 11:33:07 +1000

    Add LosslessStringConvertible conformance to Calendar.Identifier via NSCalendar.Identifier's rawValue

--- Sources/ConradEntryCore/Extensions/Calendar+ConradEntry.swift
+++ Sources/ConradEntryCore/Extensions/Calendar+ConradEntry.swift
@@ -8,3 +8,55 @@ import Foundation
 public extension Calendar {
     static let iso8601 = Calendar(identifier: .iso8601)
 }
+
+public extension Calendar.Identifier {
+    init?(_ identifier: NSCalendar.Identifier) {
+        switch identifier {
+        case .gregorian: self = .gregorian
+        case .ISO8601: self = .iso8601
+        case .buddhist: self = .buddhist
+        case .chinese: self = .chinese
+        case .coptic: self = .coptic
+        case .ethiopicAmeteAlem: self = .ethiopicAmeteAlem
+        case .ethiopicAmeteMihret: self = .ethiopicAmeteMihret
+        case .hebrew: self = .hebrew
+        case .indian: self = .indian
+        case .islamic: self = .islamic
+        case .islamicCivil: self = .islamicCivil
+        case .islamicTabular: self = .islamicTabular
+        case .islamicUmmAlQura: self = .islamicUmmAlQura
+        case .japanese: self = .japanese
+        case .persian: self = .persian
+        case .republicOfChina: self = .republicOfChina
+        default: return nil
+        }
+    }
+}
+
+extension Calendar.Identifier: LosslessStringConvertible {
+    public init?(_ description: String) {
+        self.init(NSCalendar.Identifier(rawValue: description))
+    }
+
+    public var description: String {
+        switch self {
+        case .gregorian: return NSCalendar.Identifier.gregorian.rawValue
+        case .iso8601: return NSCalendar.Identifier.ISO8601.rawValue
+        case .buddhist: return NSCalendar.Identifier.buddhist.rawValue
+        case .chinese: return NSCalendar.Identifier.chinese.rawValue
+        case .coptic: return NSCalendar.Identifier.coptic.rawValue
+        case .ethiopicAmeteAlem: return NSCalendar.Identifier.ethiopicAmeteAlem.rawValue
+        case .ethiopicAmeteMihret: return NSCalendar.Identifier.ethiopicAmeteMihret.rawValue
+        case .hebrew: return NSCalendar.Identifier.hebrew.rawValue
+        case .indian: return NSCalendar.Identifier.indian.rawValue
+        case .islamic: return NSCalendar.Identifier.islamic.rawValue
+        case .islamicCivil: return NSCalendar.Identifier.islamicCivil.rawValue
+        case .islamicTabular: return NSCalendar.Identifier.islamicTabular.rawValue
+        case .islamicUmmAlQura: return NSCalendar.Identifier.islamicUmmAlQura.rawValue
+        case .japanese: return NSCalendar.Identifier.japanese.rawValue
+        case .persian: return NSCalendar.Identifier.persian.rawValue
+        case .republicOfChina: return NSCalendar.Identifier.republicOfChina.rawValue
+        @unknown default: fatalError("Unknown calendar identifier: \(self)")
+        }
+    }
+}
