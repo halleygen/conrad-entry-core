4d8ccd26156489136ed160db862322d512b97012
Jesse Halley <halley.jesse@me.com>
2021-07-01 21:24:58 +1000

    Add required Codable function+initializer to PartialShipmentEntity since they can't be synthesised when running pre-Swift5.5

--- Sources/ConradEntryCore/Models/Other/PartialShipmentEntity.swift
+++ Sources/ConradEntryCore/Models/Other/PartialShipmentEntity.swift
@@ -56,3 +56,63 @@ extension PartialShipmentEntity.Key: LosslessStringConvertible {
 extension PartialShipmentEntity.Key: CodingKey {
     public var stringValue: String { rawValue }
 }
+
+#if swift(<5.5)
+    public extension PartialShipmentEntity {
+        init(from decoder: Decoder) throws {
+            let container = try decoder.container(keyedBy: CodingKeys.self)
+            if container.allKeys.count != 1 {
+                let context = DecodingError.Context(
+                    codingPath: container.codingPath,
+                    debugDescription: "Invalid number of keys found, expected one."
+                )
+                throw DecodingError.typeMismatch(PartialShipmentEntity.self, context)
+            }
+
+            switch container.allKeys.first.unsafelyUnwrapped {
+            case .details:
+                self = .details(try container.decode(ShipmentDetailsDTO.self, forKey: .details))
+            case .billOfLading:
+                self = .billOfLading(try container.decode(BillOfLadingDTO.self, forKey: .billOfLading))
+            case .discharge:
+                self = .discharge(try container.decode(DischargeDTO.self, forKey: .discharge))
+            case .settlementWeight:
+                self = .settlementWeight(try container.decode(DischargeWeightDTO.self, forKey: .settlementWeight))
+            case .referenceWeight:
+                self = .referenceWeight(try container.decode(DischargeWeightDTO.self, forKey: .referenceWeight))
+            case .sampleCollection:
+                self = .sampleCollection(try container.decode(SampleCollectionDTO.self, forKey: .sampleCollection))
+            case .sampleReduction:
+                self = .sampleReduction(try container.decode(SampleReductionDTO.self, forKey: .sampleReduction))
+            case .moistureDetermination:
+                self = .moistureDetermination(try container.decode(MoistureDeterminationDTO.self, forKey: .moistureDetermination))
+            case .samplePreparation:
+                self = .samplePreparation(try container.decode(QualityLotSamplePreparationDTO.self, forKey: .samplePreparation))
+            }
+        }
+
+        func encode(to encoder: Encoder) throws {
+            var container = encoder.container(keyedBy: CodingKeys.self)
+            switch self {
+            case let .details(shipmentDetailsDTO):
+                try container.encode(shipmentDetailsDTO, forKey: .details)
+            case let .billOfLading(billOfLadingDTO):
+                try container.encode(billOfLadingDTO, forKey: .billOfLading)
+            case let .discharge(dischargeDTO):
+                try container.encode(dischargeDTO, forKey: .discharge)
+            case let .settlementWeight(dischargeWeightDTO):
+                try container.encode(dischargeWeightDTO, forKey: .settlementWeight)
+            case let .referenceWeight(dischargeWeightDTO):
+                try container.encode(dischargeWeightDTO, forKey: .referenceWeight)
+            case let .sampleCollection(sampleCollectionDTO):
+                try container.encode(sampleCollectionDTO, forKey: .sampleCollection)
+            case let .sampleReduction(sampleReductionDTO):
+                try container.encode(sampleReductionDTO, forKey: .sampleReduction)
+            case let .moistureDetermination(moistureDeterminationDTO):
+                try container.encode(moistureDeterminationDTO, forKey: .moistureDetermination)
+            case let .samplePreparation(qualityLotSamplePreparationDTO):
+                try container.encode(qualityLotSamplePreparationDTO, forKey: .samplePreparation)
+            }
+        }
+    }
+#endif
