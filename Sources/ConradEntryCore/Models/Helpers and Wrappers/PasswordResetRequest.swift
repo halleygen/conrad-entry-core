32f5ef3974f9d29be53552f441a15f642b7f9537
Jesse Halley <halley.jesse@me.com>
2022-02-17 10:42:55 +1100

    Prevent initialisation of PasswordChangeRequest if passwords do not match

--- Sources/ConradEntryCore/Models/Helpers and Wrappers/PasswordResetRequest.swift
+++ Sources/ConradEntryCore/Models/Helpers and Wrappers/PasswordResetRequest.swift
@@ -3,11 +3,31 @@
 // Copyright © 2022 Jesse Halley. All rights reserved.
 //
 
-public struct PasswordChangeRequest: Codable {
+public struct PasswordChangeRequest {
     public let password: String
-    public let confirmPassword: String
+    private let confirmPassword: String
 
-    public init(password: String, confirmPassword: String) {
+    public init?(password: String, confirmPassword: String) {
+        guard password == confirmPassword else { return nil }
+        self.password = password
+        self.confirmPassword = confirmPassword
+    }
+    
+    enum CodingKeys: String, CodingKey {
+        case password, confirmPassword
+    }
+}
+
+extension PasswordChangeRequest: Encodable {}
+
+extension PasswordChangeRequest: Decodable {
+    public init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        let password = try container.decode(String.self, forKey: .password)
+        let confirmPassword = try container.decode(String.self, forKey: .confirmPassword)
+        guard password == confirmPassword else {
+            throw DecodingError.dataCorruptedError(forKey: .confirmPassword, in: container, debugDescription: "Passwords did not match.")
+        }
         self.password = password
         self.confirmPassword = confirmPassword
     }
