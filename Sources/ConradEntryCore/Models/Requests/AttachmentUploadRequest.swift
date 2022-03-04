2f472c7cd7e06e7f7090328c0c2ff5e4c39dd0f0
Jesse Halley <halley.jesse@me.com>
2022-03-04 13:46:09 +1100

    Remove shipmentID property from AttachmentUploadRequest since it belongs in URL

--- Sources/ConradEntryCore/Models/Requests/AttachmentUploadRequest.swift
+++ Sources/ConradEntryCore/Models/Requests/AttachmentUploadRequest.swift
@@ -9,15 +9,13 @@ public struct AttachmentUploadRequest: Codable {
     public let role: AttachmentRole
     public let date: Date
     public let ext: String
-    public let shipmentID: UUID
     public let md5: String
     public let comment: String
 
-    public init(role: AttachmentRole, date: Date, ext: String, shipmentID: UUID, md5: String, comment: String) {
+    public init(role: AttachmentRole, date: Date, ext: String, md5: String, comment: String) {
         self.role = role
         self.date = date
         self.ext = ext
-        self.shipmentID = shipmentID
         self.md5 = md5
         self.comment = comment
     }
