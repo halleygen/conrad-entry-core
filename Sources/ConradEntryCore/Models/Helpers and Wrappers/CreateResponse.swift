186a0b6af6396eed93e4653b0137528c3b8d83c4
Jesse Halley <halley.jesse@me.com>
2022-03-04 19:57:45 +1100

    Refactor CreateResponse to be more lightweight

--- Sources/ConradEntryCore/Models/Helpers and Wrappers/CreateResponse.swift
+++ Sources/ConradEntryCore/Models/Helpers and Wrappers/CreateResponse.swift
@@ -3,25 +3,18 @@
 // Copyright © 2022 Jesse Halley. All rights reserved.
 //
 
-public enum CreateResponse<Item: VersionedResource & Identifiable>: VersionedResource, Identifiable {
-    case created(id: Item.ID, version: Item.Version)
-    case alreadyExists(item: Item)
+public struct CreateResponse<Item: VersionedResource & Identifiable>: VersionedResource, Identifiable {
+    public let id: Item.ID
+    public let version: Item.Version
+    public let wasCreated: Bool
 
-    public var version: Item.Version {
-        switch self {
-        case let .created(_, version): return version
-        case let .alreadyExists(item): return item.version
-        }
-    }
-
-    public var id: Item.ID {
-        switch self {
-        case let .created(id, _): return id
-        case let .alreadyExists(item): return item.id
-        }
+    public init(id: Item.ID, version: Item.Version, wasCreated: Bool) {
+        self.id = id
+        self.version = version
+        self.wasCreated = wasCreated
     }
 }
 
-extension CreateResponse: Equatable where Item: Equatable, ID: Equatable, Version: Equatable {}
-extension CreateResponse: Hashable where Item: Hashable, ID: Hashable, Version: Hashable {}
+extension CreateResponse: Equatable where ID: Equatable, Version: Equatable {}
+extension CreateResponse: Hashable where ID: Hashable, Version: Hashable {}
 extension CreateResponse: APIResponse where Item: APIResponseItem {}
