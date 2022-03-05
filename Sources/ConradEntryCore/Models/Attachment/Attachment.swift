7493b4dd1a40798a358e14f002c1a27a7891fde8
Jesse Halley <halley.jesse@me.com>
2022-03-05 21:44:27 +1100

    Add VersionedResource conformance to Attachment

--- Sources/ConradEntryCore/Models/Attachment/Attachment.swift
+++ Sources/ConradEntryCore/Models/Attachment/Attachment.swift
@@ -5,18 +5,24 @@
 
 import Foundation
 
-public struct Attachment: Codable, Hashable, Identifiable {
+public struct Attachment: Codable, Hashable, Identifiable, VersionedResource {
     public var id: UUID
     public var shipmentID: UUID
     public var role: AttachmentRole
     public var date: Date
     public var comment: String?
+    public var createdAt: Date
+    public var updatedAt: Date
 
-    public init(id: UUID, shipmentID: UUID, role: AttachmentRole, date: Date, comment: String? = nil) {
+    public var version: Date { updatedAt }
+
+    public init(id: UUID, shipmentID: UUID, role: AttachmentRole, date: Date, comment: String?, createdAt: Date, updatedAt: Date) {
         self.id = id
         self.shipmentID = shipmentID
         self.role = role
         self.date = date
         self.comment = comment
+        self.createdAt = createdAt
+        self.updatedAt = updatedAt
     }
 }
