cd64d63858c9ac0e37f40cf655ef28c9b7a89c1d
Jesse Halley <halley.jesse@me.com>
2022-03-08 14:38:16 +1100

    Wrap CNPostalAddressFormatter in new FormatStyle API

---
+++ Sources/ConradEntryCore/PostalAddressStyle.swift
@@ -0,0 +1,31 @@
+//
+// ConradEntryCore
+// Copyright © 2022 Jesse Halley. All rights reserved.
+//
+
+#if canImport(Contacts)
+
+    import Contacts
+
+    public extension FormatStyle where Self == CNPostalAddress._FormatStyle {
+        static var mailingAddress: Self { Self() }
+    }
+
+    public extension CNPostalAddress {
+        struct _FormatStyle: FormatStyle {
+            public typealias FormatInput = CNPostalAddress
+            public typealias FormatOutput = String
+
+            public var style: CNPostalAddressFormatterStyle = .mailingAddress
+
+            public func format(_ value: FormatInput) -> FormatOutput {
+                CNPostalAddressFormatter.string(from: value, style: style)
+            }
+        }
+
+        func formatted(style: CNPostalAddressFormatterStyle) -> String { _FormatStyle(style: style).format(self) }
+    }
+
+    extension CNPostalAddressFormatterStyle: Codable {}
+
+#endif
