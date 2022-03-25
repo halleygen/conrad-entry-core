8557feab0b144364abcc81e67d4573de3fa9dc9b
Jesse Halley <halley.jesse@me.com>
2022-03-25 20:45:13 +1100

    Update package name

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@
 import PackageDescription
 
 let package = Package(
-    name: "ConradEntryCore",
+    name: "conradentry-core",
     defaultLocalization: "en",
     platforms: [.iOS(.v15), .macOS(.v12)],
     products: [
