927dbb2e69a0daddc801ba1f4edbc2c50639a9f3
Jesse Halley <halley.jesse@me.com>
2022-06-01 13:43:55 +1000

    Let compiler generate coding logic for CalibrationCheck Add isSingleScale property to CalibrationCheck

--- Sources/ConradEntryCore/Models/Other/CalibrationCheck.swift
+++ Sources/ConradEntryCore/Models/Other/CalibrationCheck.swift
@@ -7,27 +7,10 @@ public enum CalibrationCheck: Hashable, Codable {
     case singleScale(Result)
     case doubleScale(tare: Result, laden: Result)
 
-    public init(from decoder: Decoder) throws {
-        let container = try decoder.container(keyedBy: CodingKeys.self)
-        if let singleResult = try container.decodeIfPresent(Result.self, forKey: .singleResult) {
-            self = .singleScale(singleResult)
-        } else {
-            let tare = try container.decode(Result.self, forKey: .tareResult)
-            let laden = try container.decode(Result.self, forKey: .ladenResult)
-            self = .doubleScale(tare: tare, laden: laden)
-        }
-    }
-
-    public func encode(to encoder: Encoder) throws {
-        var container = encoder.container(keyedBy: CodingKeys.self)
-
+    public var isSingleScale: Bool {
         switch self {
-        case let .singleScale(result):
-            try container.encode(result, forKey: .singleResult)
-
-        case let .doubleScale(tare, laden):
-            try container.encode(tare, forKey: .tareResult)
-            try container.encode(laden, forKey: .ladenResult)
+        case .singleScale: return true
+        case .doubleScale: return false
         }
     }
 
@@ -41,15 +24,5 @@ public enum CalibrationCheck: Hashable, Codable {
             self.maxTonnes = maxTonnes
             self.maxVarianceKilograms = maxVarianceKilograms
         }
-
-        public enum CodingKeys: String, CodingKey {
-            case maxTonnes, maxVarianceKilograms
-        }
-    }
-
-    // MARK: - CodingKeys
-
-    public enum CodingKeys: String, CodingKey {
-        case singleResult, tareResult, ladenResult
     }
 }
