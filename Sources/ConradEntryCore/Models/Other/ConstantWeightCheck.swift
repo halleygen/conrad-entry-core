5be4523ae0e081735d4f506ef6b15ab7ff92ee26
Jesse Halley <halley.jesse@me.com>
2022-06-06 14:58:53 +1000

    Add durationInHours to ConstantWeightCheck

--- Sources/ConradEntryCore/Models/Other/ConstantWeightCheck.swift
+++ Sources/ConradEntryCore/Models/Other/ConstantWeightCheck.swift
@@ -14,3 +14,10 @@ public struct ConstantWeightCheck: Hashable, Codable {
         self.numberOfWeighBacks = numberOfWeighBacks
     }
 }
+
+public extension ConstantWeightCheck {
+    var durationInHours: Double {
+        get { Measurement<UnitDuration>(value: duration, unit: .seconds).converted(to: .hours).value }
+        set { duration = Measurement<UnitDuration>(value: newValue, unit: .hours).converted(to: .seconds).value }
+    }
+}
