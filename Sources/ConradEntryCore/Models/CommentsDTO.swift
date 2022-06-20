60cf91303750d64695dac01fa41f504c28afa710
Jesse Halley <halley.jesse@me.com>
2022-06-20 12:31:52 +1000

    Refactor CommentsDTO and massively simplify its implementation

--- Sources/ConradEntryCore/Models/CommentsDTO.swift
+++ Sources/ConradEntryCore/Models/CommentsDTO.swift
@@ -4,84 +4,25 @@
 //
 
 import Foundation
-import OrderedCollections
 
-// MARK: - Comments
+public struct CommentsGroup: Codable {
+    public struct Comment: Identifiable, Hashable, Codable {
+        public var id: UUID
+        public var value: String
+        public var version: Date?
 
-public struct CommentsDTO: ExpressibleByDictionaryLiteral {
-    public typealias Key = UUID
-    public typealias Value = String
-
-    var _base: OrderedDictionary<UUID, String>
-
-    public init() {
-        self._base = OrderedDictionary()
-    }
-
-    public init(dictionaryLiteral elements: (Key, Value)...) {
-        self._base = OrderedDictionary(uniqueKeysWithValues: elements)
-    }
-}
-
-// MARK: - Functions
-
-public extension CommentsDTO {
-    var count: Int { _base.count }
-    var isEmpty: Bool { _base.isEmpty }
-
-    subscript(id: Key) -> Value? {
-        get { _base[id] }
-        set { _base[id] = newValue }
-        _modify { yield &_base[id] }
-    }
-
-    mutating func reserveCapacity(_ minimumCapacity: Int) {
-        _base.reserveCapacity(minimumCapacity)
-    }
-}
-
-// MARK: - Sequence
-
-extension CommentsDTO: Sequence {
-    public typealias Element = OrderedDictionary<UUID, String>.Element
-    public typealias Iterator = OrderedDictionary<UUID, String>.Iterator
-
-    public var underestimatedCount: Int { _base.underestimatedCount }
-
-    public func makeIterator() -> Iterator {
-        _base.makeIterator()
-    }
-}
-
-// MARK: - Codable
-
-extension CommentsDTO: Codable {
-    private struct CodableElement: Codable {
-        let id: UUID
-        let value: String
-    }
-
-    public init(from decoder: Decoder) throws {
-        var container = try decoder.unkeyedContainer()
-
-        var idsWithComments = OrderedDictionary<UUID, String>()
-        if let count = container.count {
-            idsWithComments.reserveCapacity(count)
+        public init(id: UUID, value: String, version: Date? = nil) {
+            self.id = id
+            self.value = value
+            self.version = version
         }
-
-        while !container.isAtEnd {
-            let element = try container.decode(CodableElement.self)
-            idsWithComments[element.id] = element.value
-        }
-
-        self._base = idsWithComments
     }
 
-    public func encode(to encoder: Encoder) throws {
-        var container = encoder.unkeyedContainer()
+    public var comments: [Comment]
+    public var deletedIDs: Set<Comment.ID>
 
-        for (id, value) in _base {
-            try container.encode(CodableElement(id: id, value: value))
-        }
+    public init(comments: [Comment], deletedIDs: Set<Comment.ID> = Set()) {
+        self.comments = comments
+        self.deletedIDs = deletedIDs
     }
 }
