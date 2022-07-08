f0e9ab3c4e33800bb63fd48064627e4de344f9ed
Jesse Halley <halley.jesse@me.com>
2022-07-08 13:38:40 +1000

    Remove Codable conformance to Calendar.Identifier

--- Sources/ConradEntryCore/Extensions/CalendarIdentifier+ConradEntry.swift
+++ Sources/ConradEntryCore/Extensions/CalendarIdentifier+ConradEntry.swift
@@ -35,40 +35,30 @@ extension Calendar.Identifier: LosslessStringConvertible {
     }
 
     public var description: String {
-        switch self {
-        case .gregorian: return NSCalendar.Identifier.gregorian.rawValue
-        case .iso8601: return NSCalendar.Identifier.ISO8601.rawValue
-        case .buddhist: return NSCalendar.Identifier.buddhist.rawValue
-        case .chinese: return NSCalendar.Identifier.chinese.rawValue
-        case .coptic: return NSCalendar.Identifier.coptic.rawValue
-        case .ethiopicAmeteAlem: return NSCalendar.Identifier.ethiopicAmeteAlem.rawValue
-        case .ethiopicAmeteMihret: return NSCalendar.Identifier.ethiopicAmeteMihret.rawValue
-        case .hebrew: return NSCalendar.Identifier.hebrew.rawValue
-        case .indian: return NSCalendar.Identifier.indian.rawValue
-        case .islamic: return NSCalendar.Identifier.islamic.rawValue
-        case .islamicCivil: return NSCalendar.Identifier.islamicCivil.rawValue
-        case .islamicTabular: return NSCalendar.Identifier.islamicTabular.rawValue
-        case .islamicUmmAlQura: return NSCalendar.Identifier.islamicUmmAlQura.rawValue
-        case .japanese: return NSCalendar.Identifier.japanese.rawValue
-        case .persian: return NSCalendar.Identifier.persian.rawValue
-        case .republicOfChina: return NSCalendar.Identifier.republicOfChina.rawValue
-        @unknown default: fatalError("Unknown calendar identifier: \(self)")
-        }
+        NSCalendar.Identifier(self).rawValue
     }
 }
 
-extension Calendar.Identifier: Codable {
-    public init(from decoder: Decoder) throws {
-        let container = try decoder.singleValueContainer()
-        let rawValue = try container.decode(String.self)
-        guard let id = Self(rawValue) else {
-            throw DecodingError.dataCorrupted(.init(codingPath: [], debugDescription: "Calendar identifier '\(rawValue)' is not recognised.", underlyingError: nil))
+public extension NSCalendar.Identifier {
+    init(_ identifier: Calendar.Identifier) {
+        switch identifier {
+        case .gregorian: self = .gregorian
+        case .buddhist: self = .buddhist
+        case .chinese: self = .chinese
+        case .coptic: self = .coptic
+        case .ethiopicAmeteMihret: self = .ethiopicAmeteMihret
+        case .ethiopicAmeteAlem: self = .ethiopicAmeteAlem
+        case .hebrew: self = .hebrew
+        case .iso8601: self = .ISO8601
+        case .indian: self = .indian
+        case .islamic: self = .islamic
+        case .islamicCivil: self = .islamicCivil
+        case .japanese: self = .japanese
+        case .persian: self = .persian
+        case .republicOfChina: self = .republicOfChina
+        case .islamicTabular: self = .islamicTabular
+        case .islamicUmmAlQura: self = .islamicUmmAlQura
+        @unknown default: fatalError("Unknown calendar identifier: \(identifier)")
         }
-        self = id
-    }
-
-    public func encode(to encoder: Encoder) throws {
-        var container = encoder.singleValueContainer()
-        try container.encode(description)
     }
 }
