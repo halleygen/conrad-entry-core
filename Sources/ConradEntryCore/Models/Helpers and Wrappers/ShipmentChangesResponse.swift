8341ed911b40c60ab41c757110a862db39b71941
Jesse Halley <halley.jesse@me.com>
2022-08-15 14:27:21 +1000

    Constrain ShipmentChangesResponse.T to PartialMergable

--- Sources/ConradEntryCore/Models/Helpers and Wrappers/ShipmentChangesResponse.swift
+++ Sources/ConradEntryCore/Models/Helpers and Wrappers/ShipmentChangesResponse.swift
@@ -3,8 +3,12 @@
 // Copyright © 2022 Jesse Halley. All rights reserved.
 //
 
-public enum ShipmentChangesResponse<T: Codable>: Codable {
+public enum ShipmentChangesResponse<T: Codable & PartialMergable>: Codable {
     case noChanges
     case changes(T)
     case stale
+
+    public init(changes: T) {
+        self = changes.isEmpty ? .noChanges : .changes(changes)
+    }
 }
