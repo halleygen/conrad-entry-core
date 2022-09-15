b07ce75be0741581f346a1712e28f74fffbd089b
Jesse Halley <halley.jesse@me.com>
2022-09-15 17:01:29 +1000

    Remove unused property from ShipmentChangesRequest

--- Sources/ConradEntryCore/Models/Requests/ShipmentChangesRequest.swift
+++ Sources/ConradEntryCore/Models/Requests/ShipmentChangesRequest.swift
@@ -6,22 +6,18 @@
 import Foundation
 
 public struct ShipmentChangesRequest: Codable {
-    public let partID: UUID
     public let date: Date?
 
-    public init(partID: UUID, date: Date?) {
-        self.partID = partID
+    public init(date: Date?) {
         self.date = date
     }
 
     public enum CodingKeys: CodingKey {
-        case partID
         case date
     }
 
     public func queryItems() -> [URLQueryItem] {
         [
-            URLQueryItem(name: CodingKeys.partID.stringValue, value: partID.uuidString),
             URLQueryItem(name: CodingKeys.date.stringValue, value: (date?.timeIntervalSince1970).map(String.init(_:)))
         ]
     }
